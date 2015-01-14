969f1ae5e69e2b7a980252e8d62cb8c3055a23b0
Dewey Williams <dmw10g@my.fsu.edu>
2015-01-14 14:54:13 -0500

    Hercules TFT Example
    Cute puppy example using mibSPI on the TMS570LS0432

---
+++ TMS570LS04x_ILI9340_Example/Debug/source/sys_vim.pp
@@ -0,0 +1,39 @@
+# FIXED
+
+source/sys_vim.obj: ../source/sys_vim.c
+source/sys_vim.obj: C:/Users/Dewey/workspace_v6_0/Hercules/include/sys_vim.h
+source/sys_vim.obj: C:/Users/Dewey/workspace_v6_0/Hercules/include/reg_vim.h
+source/sys_vim.obj: C:/Users/Dewey/workspace_v6_0/Hercules/include/sys_common.h
+source/sys_vim.obj: C:/Users/Dewey/workspace_v6_0/Hercules/include/hal_stdtypes.h
+source/sys_vim.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h
+source/sys_vim.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdbool.h
+source/sys_vim.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/yvals.h
+source/sys_vim.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h
+source/sys_vim.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/linkage.h
+source/sys_vim.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/_lock.h
+source/sys_vim.obj: C:/Users/Dewey/workspace_v6_0/Hercules/include/system.h
+source/sys_vim.obj: C:/Users/Dewey/workspace_v6_0/Hercules/include/reg_system.h
+source/sys_vim.obj: C:/Users/Dewey/workspace_v6_0/Hercules/include/reg_gio.h
+source/sys_vim.obj: C:/Users/Dewey/workspace_v6_0/Hercules/include/reg_flash.h
+source/sys_vim.obj: C:/Users/Dewey/workspace_v6_0/Hercules/include/reg_tcram.h
+source/sys_vim.obj: C:/Users/Dewey/workspace_v6_0/Hercules/include/esm.h
+source/sys_vim.obj: C:/Users/Dewey/workspace_v6_0/Hercules/include/reg_esm.h
+
+../source/sys_vim.c: 
+C:/Users/Dewey/workspace_v6_0/Hercules/include/sys_vim.h: 
+C:/Users/Dewey/workspace_v6_0/Hercules/include/reg_vim.h: 
+C:/Users/Dewey/workspace_v6_0/Hercules/include/sys_common.h: 
+C:/Users/Dewey/workspace_v6_0/Hercules/include/hal_stdtypes.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdint.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdbool.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/yvals.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/linkage.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.6/include/_lock.h: 
+C:/Users/Dewey/workspace_v6_0/Hercules/include/system.h: 
+C:/Users/Dewey/workspace_v6_0/Hercules/include/reg_system.h: 
+C:/Users/Dewey/workspace_v6_0/Hercules/include/reg_gio.h: 
+C:/Users/Dewey/workspace_v6_0/Hercules/include/reg_flash.h: 
+C:/Users/Dewey/workspace_v6_0/Hercules/include/reg_tcram.h: 
+C:/Users/Dewey/workspace_v6_0/Hercules/include/esm.h: 
+C:/Users/Dewey/workspace_v6_0/Hercules/include/reg_esm.h: 
