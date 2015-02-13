5f6f3fe4217dfd8922698e03a0514725b11654a7
chrisriker <cdr11c@my.fsu.edu>
2015-02-13 00:34:19 -0500

    Update MSP430G2553_SPI_Slave.c
    Added sample code for 2-byte transmission

--- MSP430G2553 SPI Example/MSP430G2553_SPI_Slave.c
+++ MSP430G2553 SPI Example/MSP430G2553_SPI_Slave.c
@@ -9,9 +9,7 @@
 /*
  *  ======== main ========
  */
-unsigned char SLV_data = 0x00;
-unsigned int pot;
-unsigned char data;
+
 void SPI_Init( void )
 {
 	WDTCTL = WDTPW | WDTHOLD;
@@ -25,37 +23,62 @@ void SPI_Init( void )
 	__bis_SR_register(GIE);
 }
 
+/* Global data declaration/initialization */
+unsigned char SLV_data = 0x00;
+unsigned int pot;
+unsigned char data;
+int byteNum = 0;
+char sampleArray[4];
+char betweenBytes = 0;
+char matchingLSB;
+/* ====================================== */
+
 int main( void )
 {
 	SPI_Init();
 	P1SEL |= BIT0;							//Enable A/D Channel A0 (P1.0)
 
-	/*ADC10CTL0 &= ~ENC;
+	ADC10CTL0 &= ~ENC;
 	ADC10CTL0 = SREF_1 + ADC10SHT_3 + REF2_5V + REFON + ADC10IE + ADC10ON;
-    ADC10CTL1 = ADC10DIV_3 + INCH_0 + SHS_0 + CONSEQ_2 + ADC10SSEL_2;
+    	ADC10CTL1 = ADC10DIV_3 + INCH_0 + SHS_0 + CONSEQ_2 + ADC10SSEL_2;
 	ADC10CTL0 |= ENC + ADC10SC;
 
-	//while (!(ADC10IFG & ADC10CTL0)) {}
-    //pot = ADC10MEM;
+	while (!(ADC10IFG & ADC10CTL0)) {}
+    	pot = ADC10MEM;
+    	sampleArray[ byteNum ] = ( pot >> 8 );
+    	byteNum = (++byteNum) % 4;
+    	sampleArray[ byteNum ] = ( pot & 0x00FF );
+    	byteNum = (++byteNum) % 4;
 
-	/*while ( 1 ) {
+	while ( 1 ) {
+		
 		ADC10CTL0 &= ~(ADC10SC);
 		ADC10CTL0 |= ENC + ADC10SC;
-
 		while( !(ADC10CTL0 & ADC10IFG) ) {};
 		pot = ADC10MEM;
-		//ADC10CTL0 |= ENC + ADC10SC;
-	}*/
+			
+		if ( betweenBytes == 0 ) {
+		        /*  !!!!!  BEGIN DANGER ZONE  !!!!!   */
+			sampleArray[ byteNum ] = ( pot >> 8 );
+			UCB0TXBUF = sampleArray[ byteNum ];
+			matchingLSB = byteNum + 1;
+			byteNum = (++byteNum) % 4;
+			sampleArray[ byteNum ] = ( pot & 0x00FF );
+			byteNum = (++byteNum) % 4;
+			/*  !!!!!  END DANGER ZONE   !!!!!   */
+		}
+	}
     while ( 1 );
-    return (0);
+    return ( 0 );
 }
 
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR_HOOK (void)
 {
+	betweenBytes ^= 1;			//flip the bit
 	while (!(IFG2 & UCB0RXIFG)) {};
 	data = UCB0RXBUF;
 	while (!(IFG2 & UCB0TXIFG)) {};
-	UCB0TXBUF = SLV_data++;
-	__delay_cycles(50);		//change this to something way lower
+	UCB0TXBUF = sampleArray[ matchingLSB ];
+	__delay_cycles(50);
 }
