fb5b61958914e1450f5b7c9d1e7a17d35e4c3b54
Hebe <hp09d@my.fsu.edu>
2015-02-22 18:21:31 -0500

    Hall effect module counts rotations

---
+++ MSP430 Hall Effect/Debug/main.pp
@@ -0,0 +1,19 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+main.obj: c:/ti/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/intrinsics.h
+main.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/intrinsics_legacy_undefs.h
+main.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/stdio.h
+main.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/linkage.h
+main.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/stdarg.h
+
+../main.c: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/intrinsics.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/intrinsics_legacy_undefs.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/stdio.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/linkage.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.2/include/stdarg.h: 
---
+++ MSP430 Hall Effect/main.c
@@ -0,0 +1,90 @@
+/*
+ * ======== Standard MSP430 includes ========
+ */
+#include <msp430g2553.h>
+#include <stdio.h>
+/*
+ * ======== Grace related declaration ========
+ */
+
+/*
+ *  ======== main ========
+ */
+
+void SPI_Init( void )
+{
+	P1SEL |= BIT0 | BIT4 | BIT5 | BIT6 | BIT7;
+	P1SEL2 |= BIT4 | BIT5 | BIT6 | BIT7;
+
+	UCB0CTL1 |= UCSWRST;
+	UCB0CTL0 = UCCKPL | UCMSB | UCSYNC | UCMODE_2;
+	UCB0CTL1 &= ~UCSWRST;
+	IE2 |= UCB0RXIE;
+	__bis_SR_register(GIE);
+}
+
+/* Global data declaration/initialization */
+unsigned char SLV_data = 0x00;
+unsigned int pot;
+unsigned char data;
+int byteNum = 0;
+char sampleArray[4];
+char betweenBytes = 0;
+char matchingLSB;
+int num = 0;
+/* ====================================== */
+
+int main( void )
+{
+	WDTCTL = WDTPW | WDTHOLD;
+	//SPI_Init();
+	P1SEL |= BIT0;							//Enable A/D Channel A0 (P1.0)
+
+	ADC10CTL0 &= ~ENC;
+	ADC10CTL0 = SREF_1 + ADC10SHT_3 + REF2_5V + REFON + ADC10IE + ADC10ON;
+    	ADC10CTL1 = ADC10DIV_3 + INCH_0 + SHS_0 + CONSEQ_2 + ADC10SSEL_2;
+	//ADC10CTL0 |= ENC + ADC10SC;
+
+	while ( 1 ) {
+		//__delay_cycles(15000);
+		ADC10CTL0 |= ENC + ADC10SC;
+		while( !(ADC10CTL0 & ADC10IFG) ) {};
+
+		pot = ADC10MEM;
+
+		//Rotation count. Increments one per rotation regardless of clock cycle speed
+		if(pot >= 700){
+				num++;
+				while(ADC10MEM >= 600)
+				{
+					ADC10CTL0 &= ~(ADC10SC);
+					ADC10CTL0 |= ENC + ADC10SC;
+				}
+		}
+		if ( betweenBytes == 0 ) {
+		        /*  !!!!!  BEGIN DANGER ZONE  !!!!!   */
+			/*sampleArray[ byteNum ] = ( pot >> 8 );
+			UCB0TXBUF = sampleArray[ byteNum ];
+			matchingLSB = byteNum + 1;
+			byteNum = (++byteNum) % 4;
+			sampleArray[ byteNum ] = ( pot & 0x00FF );
+			byteNum = (++byteNum) % 4;*/
+			/*  !!!!!  END DANGER ZONE   !!!!!   */
+		}
+		ADC10CTL0 &= ~(ADC10SC);
+	//delay(10000);
+	}
+    while ( 1 );
+    return ( 0 );
+}
+
+/*#pragma vector=USCIAB0RX_VECTOR
+__interrupt void USCI0RX_ISR_HOOK (void)
+{
+	betweenBytes ^= 1;			//flip the bit
+	while (!(IFG2 & UCB0RXIFG)) {};
+	data = UCB0RXBUF;
+	while (!(IFG2 & UCB0TXIFG)) {};
+	UCB0TXBUF = sampleArray[ matchingLSB ];
+	__delay_cycles(50);
+}*/
